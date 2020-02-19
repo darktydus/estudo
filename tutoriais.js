a0ab681cacd7e54f94f412e2af96b9c1cd115800
darktydus <bruleorn@gmail.com>
2020-02-19 18:07:00 -0300

    tutoriais

---
+++ tutoriais.js
@@ -0,0 +1,12 @@
+//Tutorial justifyContent - https://developer.mozilla.org/en-US/docs/Web/CSS/justify-content 
+// justifyContent é usado para organizar os elementos no eixo principal
+
+//Tutorial flexDirection - https://developer.mozilla.org/en-US/docs/Web/CSS/flex-direction
+
+//Tutorial alignItens - https://developer.mozilla.org/en-US/docs/Web/CSS/align-items
+// alignItens é usado para organizar os elementos no eixo transversal (filho)
+
+//flex: é aplicada a itens que estão dentro de um flexbox dentro de uma <View>, ele serve como uma fração
+//caso tenha 3 itens, e adicione flex: 1, para um deles, ele irá utilizar 1/1 (um inteiro) do espaço
+//disponível/possível para ele. Caso adicione flex: 1 para um segundo, será agora 1/2, ou seja o flexbox
+//será dividido, e assim sucessivalmente 1/3 /, 1/4
