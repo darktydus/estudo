464d947e946790b3852832d693e38254e7884e65
Darktydus <bruleorn@gmail.com>
2020-03-04 19:43:34 -0300

    configurando vscode

--- App.js
+++ App.js
@@ -1,65 +1,62 @@
-import React,
-{ useState
-} from 'react';
-import 
-{ StyleSheet,
-  Text, 
-  View, 
-  TextInput, 
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
   Button,
-  ScrollView } from 'react-native';
+  ScrollView,
+  FlatList
+} from "react-native";
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
+  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText)
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
   };
 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...courseGoals, enteredGoal])
-  }
-
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal]);
+  };
 
   return (
-    <ScrollView>
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput 
-        placeholder= "Estabelecimento A"
-        style={styles.input} 
-        onChangeText={goalInputHandler}
-        value={enteredGoal}
-        /> 
-        <Button title= "ADD" onPress={addGoalHandler}/>  
+        <TextInput
+          placeholder="Estabelecimento A"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <ScrollView>
-      <View>
-        {courseGoals.map((goal) => 
-        <View style={styles.listItem}>
-          <Text key={goal}>{goal}</Text>
-          </View>)}
-      </View> 
-      </ScrollView>
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item}</Text>
+          </View>
+        )}
+      />
     </View>
-    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50,
+    padding: 50
   },
   inputContainer: {
     flexDirection: "row",
-    justifyContent: "space-between", 
+    justifyContent: "space-between",
     alignItems: "center"
   },
   input: {
-    width:"80%",
-    borderColor: 'black', 
-    borderWidth: 2, 
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 2,
     padding: 10
   },
   listItem: {
@@ -67,6 +64,6 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     backgroundColor: "#ccc",
     borderColor: "black",
-    borderWidth: 1,
+    borderWidth: 1
   }
 });
