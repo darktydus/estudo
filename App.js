cc1fdc4f13621ceb4f6a145aed4f3ad8b4f9a34d
Darktydus <bruleorn@gmail.com>
2020-03-11 19:24:23 -0300

    finalizando o delete goal.id !== goalId

--- App.js
+++ App.js
@@ -12,14 +12,24 @@ import {
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
+// Usando o useState, de forma padrão temos um atributo inicial, e um "set" retornando
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
+  // A ideia aqui é adicionar o atributo retornando uma ID randomica, para facilitar de adicionar ou remover futuramente
   const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle }
     ]);
+  };
+
+  //filter é um método do Javascript que permite retornar uma nova matriz, de acordo com o critério do filtro
+  // A ideia aqui é
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
   };
 
   //onAddGoalHandler recebe uma prop dentro GoalInput, então colocamos no GoalInput props.onAddGoal
@@ -29,7 +39,12 @@ export default function App() {
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={itemData => <GoalItem title={itemData.item.value} />} //itemData(?). item(objeto). value(key)
+        renderItem={itemData => (
+          <GoalItem
+            onDelete={() => console.log("Isso não funcionou?")}
+            title={itemData.item.value}
+          />
+        )} //itemData(?). item(objeto). value(key)
       />
     </View>
   );
